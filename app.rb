781da4626631d41520e6a4f03db221a1ddf065a1
andreeag <andreeag@fortech.ro>
2017-03-08 09:56:01 +0200

    Add monroe framework

--- app.rb
+++ app.rb
@@ -1,36 +1,31 @@
+require_relative 'monroe'
 require_relative 'advice'
 
-class App
+class App < Monroe
 
   def call(env)
     case env['REQUEST_PATH']
     when '/'
-      ['200', {'Content-Type' => 'text/html'}, [erb(:index)]]
+      status  = '200'
+      headers = {"Content-Type" => 'text/html'}
+      response(status, headers) do
+        erb :index
+      end
     when '/advice'
       piece_of_advice = Advice.new.generate
-      [
-        '200',
-        {"Content-Type" => 'text/html'},
-        [erb(:advice, message: piece_of_advice)]
-      ]
+      status          = '200'
+      headers         = {"Content-Type" => 'text/html'}
+      response(status, headers) do
+        erb :advice, message: piece_of_advice
+      end
     else
-      [
-        '404',
-        {"Content-Type" => 'text/html', "Content-Length" => '60'},
-        [erb(:not_found)]
-      ]
+      status  = '404'
+      headers = {"Content-Type" => 'text/html', "Content-Length" => '62'}
+      response(status, headers) do
+        erb :not_found
+      end
     end
 
-  end
-
-  private
-
-  def erb(filename, local = {})
-    b = binding
-    message = local[:message]
-    path = File.expand_path("../views/#{filename}.erb", __FILE__)
-    content = File.read(path)
-    ERB.new(content).result(b)
   end
 
 end
